04c27759a5ed8ba5d58c55d258abe04c729d8cbb
Matt Colyer <matt@colyer.name>
2011-02-22 22:00:53 -0800

    Add the ability to view the log on Windows.

--- lib/smartermeter/interfaces/swing.rb
+++ lib/smartermeter/interfaces/swing.rb
@@ -7,6 +7,8 @@ module SmarterMeter
     class Swing
       include_package "java.awt"
 
+      LogFile = File.expand_path("~/.smartermeter.log")
+
       def initialize
         # TODO: Implement a way to update the settings
         #settings_item = MenuItem.new("Settings")
@@ -22,12 +24,19 @@ module SmarterMeter
           desktop.browse(uri)
         end
 
+        log_item = MenuItem.new("View Log")
+        log_item.add_action_listener do
+          LogWindow.new {}
+        end
+
         exit_item = MenuItem.new("Exit")
         exit_item.add_action_listener {java.lang.System::exit(0)}
 
 
         popup = PopupMenu.new
         popup.add(update_item)
+        popup.add(log_item)
+        popup.add(MenuItem.new("-"))
         popup.add(exit_item)
 
         image = Toolkit::default_toolkit.get_image("icons/smartermeter-16x16.png")
@@ -41,7 +50,7 @@ module SmarterMeter
       # Returns a logger like interface to log errors and warnings to.
       def log
         return @logger if @logger
-        @logger = Logger.new STDOUT
+        @logger = Logger.new(LogFile)
         @logger.level = Logger::INFO
         @logger
       end
@@ -86,6 +95,48 @@ module SmarterMeter
         end
 
         @frame = @ui.build(:args => "Smartermeter Settings")
+        @frame.defaultCloseOperation = JFrame::DISPOSE_ON_CLOSE
+      end
+    end
+
+    class LogWindow
+      include_package "javax.swing"
+
+      LogFile = File.expand_path("~/.smartermeter.log")
+
+      def initialize(&block)
+        layout = "
+            [ log_label  ]
+            [ (600,200)*log_area ]
+            [ >buttons ]
+        "
+
+        log = File.read(LogFile)
+
+        @ui = Profligacy::Swing::LEL.new(JFrame, layout) do |c,i|
+          c.log_label = JLabel.new "Log:"
+
+          textarea = JTextArea.new(log)
+          textarea.set_caret_position(textarea.get_text().length())
+          c.log_area = JScrollPane.new(textarea)
+
+          c.buttons = Profligacy::Swing::LEL.new(JPanel, "[clear|close]") do |cc,ii|
+            cc.clear = JButton.new "Clear"
+            ii.clear = { :action => proc do |t, e|
+                File.open(LogFile, "w") { |f| f.write("") }
+                textarea.text = ""
+              end
+            }
+            cc.close = JButton.new "Close"
+            ii.close = { :action => proc do |t, e|
+                @frame.dispose
+                yield
+              end
+            }
+          end.build :auto_create_container_gaps => false
+        end
+
+        @frame = @ui.build(:args => "Smartermeter Log")
         @frame.defaultCloseOperation = JFrame::DISPOSE_ON_CLOSE
       end
     end
